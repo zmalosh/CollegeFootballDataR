101870de2bad3a8450e8c9dfbc725bdf406e8a4f
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2019-08-03 14:33:35 -0400

    add get_roster

---
+++ R/get_roster.R
@@ -0,0 +1,36 @@
+#'@title Gets the current roster for a requested team
+#'
+#'@description Gets the current roster for a requested team
+#'
+#'@param team (required) Name for desired team. "Michigan" and 'Oregon State' are valid examples.
+#'
+#'@examples
+#'
+#'@export
+
+get_roster <- function(team){
+	source('R/get_json_from_url.R')
+	queryParams <- get_roster_query_parameters(team)
+	rawRoster <- get_json_from_url('roster', queryParams = queryParams)
+	roster <- data.frame(PlayerId = rawRoster$id,
+						 FirstName = rawRoster$first_name,
+						 LastName = rawRoster$last_name,
+						 Weight = rawRoster$weight,
+						 Height = rawRoster$height,
+						 JerseyNumber = rawRoster$jersey,
+						 SchoolYear = rawRoster$year,
+						 Position = rawRoster$position,
+						 HomeCity = rawRoster$home_city,
+						 HomeState = rawRoster$home_state,
+						 HomeCountry = rawRoster$home_country,
+						 stringsAsFactors = FALSE)
+	return(roster)
+}
+
+get_roster_query_parameters <- function(team){
+	if(is.null(team)){
+		stop('Team must be provided.')
+	}
+	queryParams <- list(team = team)
+	return(queryParams)
+}
