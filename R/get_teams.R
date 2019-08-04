ce517e9b1c5fdb2e47a0c36397c94d9fedb72c3f
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2019-08-04 18:37:49 -0400

    add get_teams

---
+++ R/get_teams.R
@@ -0,0 +1,32 @@
+#'@title Gets a set of college football teams
+#'
+#'@description Get all of the teams that meet the provided criteria
+#'
+#'@param conference (optional) Abbreviation for conference. "B1G", "PAC", and "CUSA" are valid examples.
+#'
+#'@examples
+#'
+#'@export
+
+get_teams <- function(){
+	source('R/get_json_from_url.R')
+	rawTeams <- get_json_from_url('teams')
+	teams <- data.frame(SchoolName = rawTeams$school,
+						Mascot = rawTeams$mascot,
+						Conference = rawTeams$conference,
+						ConferenceDivision = rawTeams$division,
+						PrimaryColor = rawTeams$color,
+						AltColor = rawTeams$alt_color,
+						SchoolAbbr = rawTeams$abbreviation,
+						Logo = as.character(lapply(rawTeams$logos, function(x){x[1]})),
+						LogoDark = as.character(lapply(rawTeams$logos, function(x){x[2]})),
+						stringsAsFactors = FALSE)
+	return(teams)
+}
+
+get_teams_query_parameters <- function(conference){
+	if(is.null(conference)){
+		return(NULL)
+	}
+	return(list(conference = conference))
+}
