2468bf24204066e520152d0a42e2259809ce4715
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2019-08-04 22:19:50 -0400

    add get_venues

---
+++ R/get_venues.R
@@ -0,0 +1,26 @@
+#'@title Gets all available venue data
+#'
+#'@description Gets all available venue data
+#'
+#'@examples
+#'
+#'@export
+
+get_venues <- function(){
+	source('R/get_json_from_url.R')
+	rawVenues <- get_json_from_url('venues')
+	venues <- data.frame(VenueId = rawVenues$id,
+						 VenueName = rawVenues$name,
+						 Capacity = rawVenues$capacity,
+						 City = rawVenues$city,
+						 State = rawVenues$state,
+						 Country = rawVenues$country_code,
+						 ZipCode = rawVenues$zip,
+						 Longitude = rawVenues$location$x,
+						 Latitude = rawVenues$location$y,
+						 Elevation = rawVenues$elevation,
+						 YearConstructed = rawVenues$year_constructed,
+						 IsDome = rawVenues$dome,
+						 IsGrass = rawVenues$grass)
+	return(venues)
+}
